6ee4c135870b31d9baad6d8472e8e07fac301d69
Nyssa Keller <nyssa1@gmail.com>
2018-04-16 17:10:06 -0600

    Add icons to history and review screen tabs

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 class HistoryScreen extends Component {
+  static navigationOptions = {
+    title: 'History',
+    tabBarIcon:  ({ tintColor }) => {
+      return <Icon name='sort-variant' size={30} color={tintColor} />;
+    }
+  }
   render() {
     return (
       <View>
