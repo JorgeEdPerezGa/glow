cc743d795898409751c2c3cc3184c9ecdcfb6982
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-04-17 10:56:33 -0600

    about app

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,111 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, SectionList, Linking, Image, Dimensions } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class SettingScreen extends Component {
   render() {
     return (
-      <View>
-          <Text>Setting Screen</Text>
-      </View>
+      <ScrollView>
+        <Text style={styles.titleText}>GLOW</Text>
+        <Text style={styles.paragraphText}>is a mobile application that provides a user's clinician the ability to accurately measure and record anxiety and depression related disorders', based on an individuals state in the PHQ-9 scale.</Text>
+        <Text style={styles.paragraphText}>The PHQ-9, created by Dr. Kurt Kroenke, is a validated, 9-question tool to assess for the degree of depression present in an individual; the questions are not scored, but is useful functionally to help the clinician assess the impact of the patient's symptoms on his or her life.</Text>
+        <Text style={styles.paragraphText}>In addition to making criteria-based diagnoses of depressive disorders, the PHQ-9 is also a reliable and valid measure of depression severity. These characteristics plus its brevity make the PHQ-9 a useful clinical and research tool.</Text>
+        <Text style={styles.paragraphText}>Anxiety disorders may develop from a complex set of risk factors, including genetics, brain chemistry, personality, and life events.</Text>
+
+        <Text style={styles.sourceText}>To view Dr. Kurt Kroenke's publications, visit</Text>
+        <Text style={[styles.sourceText, {color: 'blue', marginTop: 0}]}
+          onPress={() => Linking.openURL('https://www.ncbi.nlm.nih.gov/pubmed/?term=Kroenke+K%5BAuthor%5D/')}>
+          {'PubMed'}
+        </Text>
+
+        <Text style={styles.paragraphTextTitle}>CONTRIBUTORS</Text>
+
+        <Image
+          style={styles.image}
+          source={{uri: 'https://avatars0.githubusercontent.com/u/25073768?s=460&v=4'}}
+        />
+        <Text style={styles.imageText}>Jorge Ed PerezGa</Text>
+
+        <Image
+          style={styles.image}
+          source={{uri: 'https://avatars1.githubusercontent.com/u/31218316?s=460&v=4'}}
+        />
+        <Text style={styles.imageText}>Nyssa Keller</Text>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 27,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 50,
+    marginBottom: 40,
+    // color: '#1c5068'
+  },
+  paragraphText: {
+    fontSize: 15,
+    marginTop: 0,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 20,
+    // color: '#1c4f67'
+  },
+  paragraphText2: {
+    fontSize: 15,
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 50,
+    // color: '#1c4f67'
+  },
+  paragraphTextTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 30,
+    marginLeft: 20,
+    // color: '#1c4f67'
+  },
+  sourceText: {
+    fontSize: 12,
+    textAlign: 'center',
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 10
+  },
+  item: {
+    fontSize: 18,
+    marginTop: 0,
+    marginLeft: 20,
+    marginBottom: 20,
+    color: '#3d3c3c'
+  },
+  image: {
+    width: 400,
+    height: 400,
+    margin: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  imageText: {
+    fontSize: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    fontWeight: 'bold'
+    // color: '#1c4f67'
+  },
+  iconSet: {
+    flex: 1
+  },
+  iconImage: {
+    width: 50,
+    height: 50,
+  }
+});
 
 export default SettingScreen;
