be86f954d541fb682044cf268f59dbcb710b54cc
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-04-17 18:58:16 -0600

    add alternate color images

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -6,16 +6,66 @@ import Deck from '../src/Deck';
 const MARGIN_TOP = '8%';
 
 const data = [
-  { id: 1, title:'1/10', text: 'Little interest or pleasure in doing things?', uri: 'https://images.pexels.com/photos/623919/pexels-photo-623919.jpeg?auto=compress&cs=tinysrgb&h=350' },
-  { id: 2, title:'2/10', text: 'Feeling down, depressed, or hopeless?', uri: 'https://images.pexels.com/photos/127260/pexels-photo-127260.jpeg?auto=compress&cs=tinysrgb&h=350' },
-  { id: 3, title:'3/10', text: 'Trouble falling or staying asleep, or sleeping too much?', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
-  { id: 4, title:'4/10', text: 'Feeling tired or having little energy?', uri: 'https://images.pexels.com/photos/863988/pexels-photo-863988.jpeg?auto=compress&cs=tinysrgb&h=350' },
-  { id: 5, title:'5/10', text: 'Poor appetite or overeating?', uri: 'https://images.pexels.com/photos/70861/pexels-photo-70861.jpeg?auto=compress&cs=tinysrgb&h=350' },
-  { id: 6, title:'6/10', text: 'Feeling bad about yourself — or that you are a failure or have let yourself or your family down?', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
-  { id: 7, title:'7/10', text: 'Trouble concentrating on things, such as reading the newspaper or watching television?', uri: 'https://images.pexels.com/photos/518543/pexels-photo-518543.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' },
-  { id: 8, title:'8/10', text: 'Moving or speaking so slowly that other people could have noticed? Or so fidgety or restless that you have been moving a lot more than usual?', uri: 'https://images.pexels.com/photos/306253/pexels-photo-306253.jpeg?auto=compress&cs=tinysrgb&h=350' },
-  { id: 9, title:'9/10', text: 'Thoughts that you would be better off dead, or thoughts of hurting yourself in some way?', uri: 'https://images.pexels.com/photos/967933/pexels-photo-967933.jpeg?auto=compress&cs=tinysrgb&h=350' },
-  { id: 10, title:'10/10', text: 'How difficult have these problems made it for you to do your work, take care of things at home, or get along with other people?', uri: 'https://images.pexels.com/photos/942002/pexels-photo-942002.jpeg?auto=compress&cs=tinysrgb&h=350' },
+  { id: 1, title:'1/10', text: 'Little interest or pleasure in doing things?', uri:
+
+  //'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fi.imgur.com%2FxTz0KsB.png'
+
+  'https://images.pexels.com/photos/623919/pexels-photo-623919.jpeg?auto=compress&cs=tinysrgb&h=350'
+ },
+  { id: 2, title:'2/10', text: 'Feeling down, depressed, or hopeless?',
+  uri:
+
+  //'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fi.imgur.com%2FuXykmXZ.png'
+
+  'https://images.pexels.com/photos/127260/pexels-photo-127260.jpeg?auto=compress&cs=tinysrgb&h=350'
+},
+  { id: 3, title:'3/10', text: 'Trouble falling or staying asleep, or sleeping too much?', uri:
+
+  //'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fi.imgur.com%2FXQmRVyH.png'
+
+  'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg'
+},
+  { id: 4, title:'4/10', text: 'Feeling tired or having little energy?', uri:
+
+  //'https://i.imgur.com/lwRXYg3.png'
+
+  'https://images.pexels.com/photos/863988/pexels-photo-863988.jpeg?auto=compress&cs=tinysrgb&h=350'
+},
+  { id: 5, title:'5/10', text: 'Poor appetite or overeating?', uri:
+
+  //'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fi.imgur.com%2FLjABTSk.png'
+
+   'https://images.pexels.com/photos/70861/pexels-photo-70861.jpeg?auto=compress&cs=tinysrgb&h=350'
+},
+  { id: 6, title:'6/10', text: 'Feeling bad about yourself — or that you are a failure or have let yourself or your family down?', uri:
+  //'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fi.imgur.com%2Foyp2bXo.png'
+
+   'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg'
+},
+  { id: 7, title:'7/10', text: 'Trouble concentrating on things, such as reading the newspaper or watching television?', uri:
+
+  //'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fi.imgur.com%2F92tpFtm.png'
+
+  'https://images.pexels.com/photos/518543/pexels-photo-518543.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+},
+  { id: 8, title:'8/10', text: 'Moving or speaking so slowly that other people could have noticed? Or so fidgety or restless that you have been moving a lot more than usual?', uri:
+
+  //'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fi.imgur.com%2FSD84YzT.png'
+
+  'https://images.pexels.com/photos/306253/pexels-photo-306253.jpeg?auto=compress&cs=tinysrgb&h=350'
+},
+  { id: 9, title:'9/10', text: 'Thoughts that you would be better off dead, or thoughts of hurting yourself in some way?', uri:
+
+  //'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fi.imgur.com%2F5G7V1q7.png'
+
+  'https://images.pexels.com/photos/967933/pexels-photo-967933.jpeg?auto=compress&cs=tinysrgb&h=350'
+},
+  { id: 10, title:'10/10', text: 'How difficult have these problems made it for you to do your work, take care of things at home, or get along with other people?', uri:
+
+  //'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fi.imgur.com%2FNFX7wmj.png'
+
+  'https://images.pexels.com/photos/942002/pexels-photo-942002.jpeg?auto=compress&cs=tinysrgb&h=350'
+},
 ];
 
 class DeckScreen extends Component {
--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, SectionList, Linking } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, SectionList, Linking, Image, Dimensions } from 'react-native';
 import { Button, Icon } from 'react-native-elements';
 
 class ReviewScreen extends Component {
@@ -11,7 +11,7 @@ class ReviewScreen extends Component {
       // title: 'ABOUT',
       headerRight: (
         <Button
-          title = "ABOUT GLOW"
+          title = "MORE ABOUT ANXIETY"
           onPress = {() => navigate('settings')}
           backgroundColor="rgba(0,0,0,0)"
           color="rgba(0, 122, 255, 1)"
@@ -29,38 +29,31 @@ class ReviewScreen extends Component {
   render() {
     return (
       <ScrollView>
-        <Text style={styles.titleText}>ABOUT ANXIETY DISORDERS</Text>
-        <Text style={styles.paragraphText}>Anxiety disorders are real, serious medical conditions - just as real and serious as physical disorders such as heart disease or diabetes. Anxiety disorders are the most common and pervasive mental disorders in the United States.</Text>
-        <Text style={styles.paragraphText}>The term "anxiety disorder" refers to specific psychiatric disorders that involve extreme fear or worry, and includes:</Text>
-        <SectionList
-          sections={[
-            {title: '',
-              data: [
-                '• generalized anxiety disorder (GAD)',
-                '• panic disorder and panic attacks',
-                '• agoraphobia',
-                '• social anxiety disorder',
-                '• selective mutism',
-                '• separation anxiety',
-                '• specific phobias'
-              ]},
-              {title: '',
-                data: [
-                  // 'Obsessive-compulsive disorder (OCD)',
-                  // 'posttraumatic stress disorder (PTSD)'
-                ]},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-          keyExtractor={(item, index) => index}
-        />
-        <Text style={styles.paragraphText}>Obsessive-compulsive disorder (OCD) and posttraumatic stress disorder (PTSD) are closely related to anxiety disorders, which some may experience at the same time as depression.</Text>
+        <Text style={styles.titleText}>GLOW</Text>
+        <Text style={styles.paragraphText}>is a mobile application that provides a user's clinician the ability to accurately measure and record anxiety and depression related disorders', based on an individuals state in the PHQ-9 scale.</Text>
+        <Text style={styles.paragraphText}>The PHQ-9, created by Dr. Kurt Kroenke, is a validated, 9-question tool to assess for the degree of depression present in an individual; the questions help the clinician assess the impact of the patient's symptoms on his or her life.</Text>
+        <Text style={styles.paragraphText}>In addition to making criteria-based diagnoses of depressive disorders, the PHQ-9 is also a reliable and valid measure of depression severity. These characteristics plus its brevity make the PHQ-9 a useful clinical and research tool.</Text>
         <Text style={styles.paragraphText}>Anxiety disorders may develop from a complex set of risk factors, including genetics, brain chemistry, personality, and life events.</Text>
-        <Text style={styles.sourceText}>information provided by</Text>
+
+        <Text style={styles.sourceText}>To view Dr. Kurt Kroenke's publications, visit</Text>
         <Text style={[styles.sourceText, {color: 'blue', marginTop: 0}]}
-          onPress={() => Linking.openURL('https://adaa.org/')}>
-          {' Anxiety and Depression Association of America'}
+          onPress={() => Linking.openURL('https://www.ncbi.nlm.nih.gov/pubmed/?term=Kroenke+K%5BAuthor%5D/')}>
+          {'PubMed'}
         </Text>
+
+        <Text style={styles.paragraphTextTitle}>CONTRIBUTORS</Text>
+
+        <Image
+          style={styles.image}
+          source={{uri: 'https://avatars0.githubusercontent.com/u/25073768?s=460&v=4'}}
+        />
+        <Text style={styles.imageText}>Jorge Ed PerezGa</Text>
+
+        <Image
+          style={styles.image}
+          source={{uri: 'https://avatars1.githubusercontent.com/u/31218316?s=460&v=4'}}
+        />
+        <Text style={styles.imageText}>Nyssa Keller</Text>
       </ScrollView>
     )
   }
@@ -71,16 +64,31 @@ const styles = StyleSheet.create({
     fontSize: 27,
     fontWeight: 'bold',
     textAlign: 'center',
-    margin: 45,
+    margin: 50,
     marginBottom: 40,
     // color: '#1c5068'
   },
   paragraphText: {
-    fontSize: 20,
+    fontSize: 15,
     marginTop: 0,
     marginLeft: 20,
     marginRight: 20,
     marginBottom: 20,
+    // color: '#1c4f67'
+  },
+  paragraphText2: {
+    fontSize: 15,
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 50,
+    // color: '#1c4f67'
+  },
+  paragraphTextTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 30,
+    marginLeft: 20,
     // color: '#1c4f67'
   },
   sourceText: {
@@ -97,6 +105,27 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginBottom: 20,
     color: '#3d3c3c'
+  },
+  image: {
+    width: 400,
+    height: 400,
+    margin: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  imageText: {
+    fontSize: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    fontWeight: 'bold'
+    // color: '#1c4f67'
+  },
+  iconSet: {
+    flex: 1
+  },
+  iconImage: {
+    width: 50,
+    height: 50,
   }
 });
 
