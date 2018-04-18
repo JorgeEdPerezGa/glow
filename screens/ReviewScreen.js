26a6ae3fd7c0f1c744f3843229e7b99b0723d03f
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-04-17 20:50:57 -0600

    style about

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -28,7 +28,7 @@ class ReviewScreen extends Component {
 
   render() {
     return (
-      <ScrollView>
+      <ScrollView style={{backgroundColor: '#292b31'}}>
         <Text style={styles.titleText}>GLOW</Text>
         <Text style={styles.paragraphText}>is a mobile application that provides a user's clinician the ability to accurately measure and record anxiety and depression related disorders', based on an individuals state in the PHQ-9 scale.</Text>
         <Text style={styles.paragraphText}>The PHQ-9, created by Dr. Kurt Kroenke, is a validated, 9-question tool to assess for the degree of depression present in an individual; the questions help the clinician assess the impact of the patient's symptoms on his or her life.</Text>
@@ -36,7 +36,7 @@ class ReviewScreen extends Component {
         <Text style={styles.paragraphText}>Anxiety disorders may develop from a complex set of risk factors, including genetics, brain chemistry, personality, and life events.</Text>
 
         <Text style={styles.sourceText}>To view Dr. Kurt Kroenke's publications, visit</Text>
-        <Text style={[styles.sourceText, {color: 'blue', marginTop: 0}]}
+        <Text style={[styles.sourceText, {color: '#abdce8', marginTop: 0}]}
           onPress={() => Linking.openURL('https://www.ncbi.nlm.nih.gov/pubmed/?term=Kroenke+K%5BAuthor%5D/')}>
           {'PubMed'}
         </Text>
@@ -64,40 +64,62 @@ const styles = StyleSheet.create({
     fontSize: 27,
     fontWeight: 'bold',
     textAlign: 'center',
-    margin: 50,
+    width: 380,
     marginBottom: 40,
-    // color: '#1c5068'
+    marginTop: 0,
+    fontWeight: '400',
+    color: '#fff',
+    padding: 20,
+    backgroundColor: '#ed5c8f',
+    shadowOffset:{ width: 0, height: 20, },
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
   },
   paragraphText: {
-    fontSize: 15,
+    fontSize: 20,
     marginTop: 0,
     marginLeft: 20,
     marginRight: 20,
     marginBottom: 20,
-    // color: '#1c4f67'
+    color: '#c9c9c9',
+    fontWeight: '500'
   },
-  paragraphText2: {
-    fontSize: 15,
-    marginTop: 30,
-    marginLeft: 20,
-    marginRight: 20,
-    marginBottom: 50,
-    // color: '#1c4f67'
-  },
+  // paragraphText2: {
+  //   fontSize: 15,
+  //   marginTop: 30,
+  //   marginLeft: 20,
+  //   marginRight: 20,
+  //   marginBottom: 50,
+  //   // color: '#1c4f67'
+  // },
   paragraphTextTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    margin: 30,
-    marginLeft: 20,
-    // color: '#1c4f67'
+    fontSize: 27,
+    fontWeight: '400',
+    marginBottom: 10,
+    color: '#ccc',
+    textAlign: 'center',
+      fontSize: 27,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      width: 380,
+      marginBottom: 40,
+      marginTop: 40,
+      fontWeight: '400',
+      color: '#fff',
+      padding: 20,
+      backgroundColor: '#6150ce',
+      shadowOffset:{ width: 0, height: 20, },
+      shadowColor: 'black',
+      shadowOpacity: 0.3,
   },
   sourceText: {
-    fontSize: 12,
-    textAlign: 'center',
+    fontSize: 14,
+    // textAlign: 'center',
     marginTop: 30,
     marginLeft: 20,
     marginRight: 20,
-    marginBottom: 10
+    marginBottom: 10,
+    color: '#ccc'
   },
   item: {
     fontSize: 18,
@@ -117,8 +139,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginLeft: 'auto',
     marginRight: 'auto',
-    fontWeight: 'bold'
-    // color: '#1c4f67'
+    fontWeight: 'bold',
+    color: '#ccc'
   },
   iconSet: {
     flex: 1
